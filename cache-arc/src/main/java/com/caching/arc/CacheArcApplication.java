5326150ef4f63c7b07f258aed98d83e431ab0868
rahul <88mydestiny@gmail.com>
2026-04-25 19:12:03 +0530

    cache initial commit

---
+++ cache-arc/src/main/java/com/caching/arc/CacheArcApplication.java
@@ -0,0 +1,13 @@
+package com.caching.arc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheArcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheArcApplication.class, args);
+	}
+
+}
